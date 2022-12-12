2629c05c0e883ae0c776e5b595e3d324a09c3705
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-12-12 21:20:24 +0600

    default

---
+++ arrow-function.js
@@ -0,0 +1,13 @@
+function add(num1, num2){
+    return num1 + num2;
+     
+}
+const add2 = function add2(num1, num2){
+    return num1 +  num2;
+}
+const sum1 = add(15, 17);
+const sum2 = add2(15,17);
+console.log(sum1, sum2);
+// document.getElementById('my-btn').onclick = function handleEvent(){
+
+// }
--- tamplate-string.js
+++ tamplate-string.js
@@ -4,5 +4,16 @@ const kobita =`kobita tumi sopno charini`;
 const multiline = ` this is multi line 
 this is first line 
 this is second line 
-this is third line `
-console.log(multiline);
+this is third line `;
+
+const friends = ['abul', 'kabul', 'babul', 'sabul'];
+const count = 5;
+const old = '<h3 class= "friend-name">Friend-5</h3>'
+const old2 = '<h3 class= "friend-name">Friend-' + count +'</h3>';
+const new1 = `<h3 class= "friend-name">Friend-${friends.length}</h3>`
+const first = 'Mamun';
+const last = 'Chowdhury';
+const fullOld = first + ' ' + last;
+const fullNew = ` This person name is : ${first} ${last}`;
+console.log(fullNew);
+
